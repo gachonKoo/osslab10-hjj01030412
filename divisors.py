f9093849fa109508a086b2084e11ffa207561e99
hjj01030412 <parichae0103@naver.com>
2025-11-07 22:10:11 +0900

    Refactor get_divisors function into main logic

--- divisors.py
+++ divisors.py
@@ -1,15 +1,6 @@
-import sys
-
-def get_divisors(n):
-    divisors = []
-    for i in range(1, n + 1):
-        if n % i == 0:
-            divisors.append(i)
-    return divisors
-
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        n = int(sys.argv[1])
-    else:
-        n = int(input())
-    print(get_divisors(n))
+n = int(input())
+divisors = []
+for i in range(1, n + 1):
+    if n % i == 0:
+        divisors.append(i)
+print(divisors)
