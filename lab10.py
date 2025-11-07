44c47ba470954a869d93fe676efa16771e51d52f
hjj01030412 <parichae0103@naver.com>
2025-11-07 21:56:25 +0900

    Change divisor output to list format
    Refactor divisor calculation to store results in a list.

--- lab10.py
+++ lab10.py
@@ -1,9 +1,8 @@
-# get_divisors.py
+n = int(input())
 
-# 정수를 입력받아 그 수의 약수를 출력하는 프로그램
-n = int(input("Enter a number: "))
-
-print("Divisors of", n, "are:")
-for i in range(1, n + 1):
+divisors = []
+for i in range(1, n+1):
     if n % i == 0:
-        print(i)
+        divisors.append(i)
+
+print(divisors)
