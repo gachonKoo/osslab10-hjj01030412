617bb9370c17778e9fe2d8cdcfdc49351e6fad51
hjj01030412 <parichae0103@naver.com>
2025-11-07 21:59:56 +0900

    Update lab10.py

--- lab10.py
+++ lab10.py
@@ -1,8 +1,6 @@
-n = int(input())
-
-divisors = []
-for i in range(1, n+1):
-    if n % i == 0:
-        divisors.append(i)
-
-print(divisors)
+def get_divisors(n):
+    divisors = []
+    for i in range(1, n + 1):
+        if n % i == 0:
+            divisors.append(i)
+    return divisors
